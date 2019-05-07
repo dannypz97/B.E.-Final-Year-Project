a03ac763f0490cf8916fbfbc3a9fbe4eb0f94125
Daniyal Parveez <dparveez.dp@gmail.com>
2019-05-07 19:48:58 +0530

    Removed unecessary code

--- app.py
+++ app.py
@@ -17,19 +17,13 @@ graph = None
 def index():
     global table, classifier, model, embeddings_index, graph
 
-    if classifier is None and model is None and embeddings_index is None and graph is None:
-        classifier = SentenceClassifier()
-        model, embeddings_index = classifier.setup_classifier(table, load_saved=1)
-        graph = tf.get_default_graph()
-
     if request.args.get("dataset"):
         table = request.args.get("dataset")
 
-        with graph.as_default():
-            #global classifier, model, embeddings_index
-            classifier = SentenceClassifier()
-            model, embeddings_index = classifier.setup_classifier(table)
-            graph = tf.get_default_graph()
+    classifier = SentenceClassifier()
+    model, embeddings_index = classifier.setup_classifier(table, load_saved=1)
+    graph = tf.get_default_graph()
+
     return render_template('index.html', table=table)
 
 @app.route('/classify/', methods=['GET', 'POST'])
@@ -50,32 +44,7 @@ def classify():
 
 
 
-@app.route('/data/')
-def view_dataset():
-
-    mydb, cursor = classifier.connect_to_db()
-    print(table)
-    label = request.args.get("label")
-    if label is None:
-        cursor.execute('select distinct(broad_label) from tags where `table` = "' + table + '"')
-        result = cursor.fetchall()
-    else:
-        result = [(label)]
-
-    rows = []
-
-    for row in result:
-        if label is None:
-            cursor.execute('select question, tags from ' + table + ' where tags like "%' + row[0] + '%" limit 1000')
-        else:
-            cursor.execute('select question, tags from ' + table + ' where tags like "%' + row + '%" ')
-        for element in cursor.fetchall():
-            rows.append(element)
-
-    del mydb, cursor
-    return render_template('data.html', rows = rows)
-
-
 if __name__ == '__main__':
     table = 'trec'
+
     app.run(debug=True, threaded=True)
